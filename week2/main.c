c67c23e2fe5531b8a9c3c50c296b17a4d93c588c
Афанасий Иванов <afanasijivanov0@gmail.com>
2025-09-26 19:45:58 +0300

    сделано 1 и 2 задача

--- week2/main.c
+++ week2/main.c
@@ -33,6 +33,36 @@ int FindMaximum(const int* signals, int size) {
     return max;
 }
 
+// Find avg value
+ double CalculateAverage(const int* signals, int size) {
+
+    double avg = 0;
+    for (int i = 0; i < size; i++) {
+        avg+= signals[i];
+    }
+
+    avg = avg / size;
+    return avg;
+}
+
+// Find minimal index
+int FindMinimumindex(const int* signals, int size) {
+    if (size <= 0) {
+        return INT_MAX;
+    }
+    
+    int min = signals[0];
+    int minindex = 0;
+    for (int i = 1; i < size; i++) {
+        if (signals[i] < min) {
+            min = signals[i];
+            minindex = i;
+        }
+    }
+    
+    return minindex;
+}
+
 int main() {
     // Example signal data
     int signals[] = {5, -3, 10, 1, 8, -7, 4, 2};
@@ -40,7 +70,9 @@ int main() {
     
     int min_value = FindMinimum(signals, size);
     int max_value = FindMaximum(signals, size);
-    
+    double avg_value = CalculateAverage(signals, size);
+    int min_index = FindMinimumindex(signals, size);
+
     printf("Signals: ");
     for (int i = 0; i < size; i++) {
         printf("%d ", signals[i]);
@@ -49,6 +81,7 @@ int main() {
     
     printf("Minimum value: %d\n", min_value);
     printf("Maximum value: %d\n", max_value);
-    
+    printf("Average value: %f\n", avg_value);
+    printf("Index minimum: %d\n", min_index);
     return 0;
 }
